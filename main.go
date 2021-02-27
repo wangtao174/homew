4c2121bcbac2a18a9677a4c06c10d694c5855df3
wangtao174 <76892943+wangtao174@users.noreply.github.com>
2021-02-27 14:03:15 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type person struct {
+	neme string
+	age int
+	gender string
+	issingle bool
+}
+
+type  dove interface {
+	gugugu()
+}
+
+type repeater interface {
+	repeat(string)
+}
+
+type lemon_jin interface{
+	suan()
+}
+type zhenxiangguai interface {
+	jiaojiaojiao()
+}
+
+func (p *person) gugugu(){
+	fmt.Println(p.neme,"又在放鸽子")
+}
+func (p *person) repeat(word string){
+	fmt.Println(p.neme,"跟着又说了一遍\n",word)
+}
+func (p *person) suan(){
+	fmt.Println("臭柠檬精",p.neme,"又酸了")
+}
+func(p *person) jiaojiaojiao() {
+	fmt.Println(p.neme,"给了自己一个巴掌，并说:“学习go语言真香！”")
+}
+
+func main (){
+	p :=&person{"小萌新",18,"male",true}
+	q :=&person{"王二麻子",18,"male",false}
+	var w dove
+	w=q
+	w.gugugu()
+
+	var x repeater
+	x=p
+	x.repeat("“CAS的dl们tql”")
+
+	var e lemon_jin
+	e=q
+	e.suan()
+	var r zhenxiangguai
+	r=p
+	r.jiaojiaojiao()
+
+
+}
