2c74d239fbe1486b84adfb5064dccc4c0d71c4bd
wangtao174 <76892943+wangtao174@users.noreply.github.com>
2021-02-27 14:05:05 +0800

    Add files via upload

---
+++ 2.go
@@ -0,0 +1,36 @@
+
+package _main
+
+import "fmt"
+
+func receveiver (v interface{}) {
+	switch v.(type) {
+	case string:
+		fmt.Println("这个是字符串！")
+	case bool:
+		fmt.Println("这个是bool值!")
+	case int:
+		fmt.Println("这个是int类型!")
+	case float64:
+		fmt.Println("这个是float类型!")
+	case interface{}:
+		fmt.Println("这个是空接口奥！")
+	default:
+		fmt.Println("不知道这个是什么类型的值!")
+
+
+	}
+}
+
+func main (){
+	a:=32
+	b:="helly Golang"
+	c:=3.1415926
+	d:=false
+	var x interface{}
+	receveiver(a)
+	receveiver(b)
+	receveiver(c)
+	receveiver(d)
+	receveiver(x)
+}
